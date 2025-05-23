096483fe434daefec2ef3884088fce492a8b3382
developedByAn009 <95417288+An009@users.noreply.github.com>
2025-05-23 22:25:42 +0000

    adding analytics for the website

--- project/src/main.tsx
+++ project/src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
 import "./index.css";
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -12,3 +14,8 @@ createRoot(document.getElementById("root")!).render(
     </ThemeProvider>
   </StrictMode>
 );
+
+if (import.meta.env.PROD) {
+  inject();
+  injectSpeedInsights();
+}
